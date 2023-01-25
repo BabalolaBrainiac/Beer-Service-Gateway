f09c24ba94840c3c61d1c48ef39847465e7243b5
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2023-01-25 18:15:12 +0100

    updates

---
+++ src/main/java/com/babalola/beerserviceapigateway/BeerServiceApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.babalola.beerserviceapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerServiceApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerServiceApiGatewayApplication.class, args);
+    }
+
+}
